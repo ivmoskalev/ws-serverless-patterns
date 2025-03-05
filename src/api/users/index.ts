7f05433aba2a2ca983832c2f3ac854ff3e69529b
Workshop Participant <participant@workshops.aws>
2025-03-05 16:45:27 +0000

    feat: M2.2 - Add Business Logic

--- src/api/users/index.ts
+++ src/api/users/index.ts
@@ -1,28 +1,165 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
 import { 
   Context,
   APIGatewayProxyResult,
   APIGatewayEvent
 } from 'aws-lambda';
 
-/**
- * Sample pure Lambda function
- * 
- * @see https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {APIGatewayEvent} event API Gateway Lambda Proxy Input Format
- * 
- * @see https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
- * @param {Context} context Lambda Context runtime methods and attributes
- * 
- * @see https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Promise<APIGatewayProxyResult>} API Gateway Lambda Proxy Output
- */
+import {
+  DynamoDBClient,
+} from '@aws-sdk/client-dynamodb';
+
+import {
+  DeleteCommand,
+  DynamoDBDocumentClient,
+  GetCommand,
+  PutCommand,
+  ScanCommand,
+} from '@aws-sdk/lib-dynamodb';
+
+import { v4 as uuidv4 } from 'uuid';
+
+const USERS_TABLE = process.env.USERS_TABLE || '';
+const dynamoDb = new DynamoDBClient({});
+const documentClient = DynamoDBDocumentClient.from(dynamoDb);
+
+enum UserRoutes {
+  CREATE_USER = 'POST /users',
+  DELETE_USER = 'DELETE /users/{userid}',
+  GET_USER = 'GET /users/{userid}',
+  GET_USERS = 'GET /users',
+  UPDATE_USER = 'PUT /users/{userid}'
+}
+
 export const handler = (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
-  return Promise.resolve(
-    {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: 'hello world',
-      }),
-    }
-  )
+  let response: Promise<APIGatewayProxyResult>;
+    
+  switch (`${event.httpMethod} ${event.resource}`) {
+    case UserRoutes.CREATE_USER:
+      response = createUser(event);
+      break;
+    case UserRoutes.DELETE_USER:
+      response = deleteUser(event);
+      break;
+    case UserRoutes.GET_USER:
+      response = getUser(event);
+      break;
+    case UserRoutes.GET_USERS:
+      response = getUsers();
+      break;
+    case UserRoutes.UPDATE_USER:
+      response = updateUser(event);
+      break;
+    default:
+      response = Promise.resolve({
+        statusCode: 400,
+        headers: { ...defaultHeaders, },
+        body: JSON.stringify({
+          message: 'Unsupported route',
+        }),
+      });
+      break;
+  }
+
+  return response
+    .catch(error => {
+      console.log(error);
+      return {
+        statusCode: 400,
+        headers: { ...defaultHeaders, },
+        body: JSON.stringify({'Error': error}),
+      }
+    });
 };
+
+const defaultHeaders = {
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin': '*',
+};
+
+const createUser = (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
+  const body = JSON.parse(event.body || '{}');
+  body['timestamp'] = new Date().toISOString();
+  body['userid'] = uuidv4();
+
+  return documentClient.send(new PutCommand({
+    TableName: USERS_TABLE,
+    Item: {
+      ...body,
+    },
+  })).then(() => { 
+    return {
+      statusCode: 201,
+      headers: { ...defaultHeaders, },
+      body: JSON.stringify(body),
+    }
+  });
+}
+
+const deleteUser = (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
+  const { userid } = event.pathParameters || {};
+
+  return documentClient.send(new DeleteCommand({
+    TableName: USERS_TABLE,
+    Key: {
+      "userid": userid,
+    },
+  })).then(() => {
+    return {
+      statusCode: 200,
+      headers: { ...defaultHeaders, },
+      body: JSON.stringify({}),
+    }
+  });
+}
+
+const getUser = (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
+  const { userid } = event.pathParameters || {};
+
+  return documentClient.send(new GetCommand({
+    TableName: USERS_TABLE,
+    Key: {
+      "userid": userid,
+    }
+  })).then(data => {
+    return {
+      statusCode: 200,
+      headers: { ...defaultHeaders, },
+      body: JSON.stringify(data.Item || {}),
+    }
+  });
+}
+
+const getUsers = (): Promise<APIGatewayProxyResult> => {
+  return documentClient.send(new ScanCommand({
+    TableName: USERS_TABLE,
+    Select: 'ALL_ATTRIBUTES',
+  })).then(data => {
+    return {
+      statusCode: 200,
+      headers: { ...defaultHeaders, },
+      body: JSON.stringify(data.Items),
+    }
+  });
+}
+
+const updateUser = (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
+  const { userid } = event.pathParameters || {};
+  const body = JSON.parse(event.body || '{}');
+  body['timestamp'] = new Date().toISOString();
+  body['userid'] = userid;  
+
+  return documentClient.send(new PutCommand({
+    TableName: USERS_TABLE,
+    Item: {
+      ...body,
+    },
+  })).then(() => {
+    return {
+      statusCode: 200,
+      headers: { ...defaultHeaders, },
+      body: JSON.stringify(body),
+    }
+  });
+}
