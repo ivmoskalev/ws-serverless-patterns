5be7ea45cba8b4eb9b2ed68f667c2a909720f54f
Workshop Participant <participant@workshops.aws>
2025-03-05 16:20:52 +0000

    feat: M2.1 - Create data store

--- lib/ws-serverless-patterns-stack.ts
+++ lib/ws-serverless-patterns-stack.ts
@@ -1,11 +1,31 @@
-import { 
-  Stack, 
-  StackProps
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+import {
+  Aws,
+  CfnOutput,
+  RemovalPolicy,
+  Stack,
+  StackProps,
+  Tags
 } from 'aws-cdk-lib';
+
 import { Construct } from 'constructs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
 export class WsServerlessPatternsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const usersTable = new dynamodb.Table(this, 'users-table', {
+      tableName: `${Aws.STACK_NAME}-users`,
+      partitionKey: { name: 'userid', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.DESTROY
+    });
+
+    new CfnOutput(this, 'UsersTable', {
+      description: 'DynamoDB Users table',
+      value: usersTable.tableName
+    });
   }
 }
